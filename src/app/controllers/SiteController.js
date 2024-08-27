cbd2285c55ed7b68ea7789fc3e35cf98c0ac7006
huynhtri123 <huynhmtri1211@gmail.com>
2024-08-27 15:29:33 +0700

    Get all courses from db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,16 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
+    index(req, res, next) {
         Course.find({})
             .then(courses => {
-                res.json(courses);
+                res.render('home', { 
+                    courses: multipleMongooseToObject(courses)
+                });
             })
-            .catch(err => {
-                console.log(err);
-                res.status(400).json({error: "ERROR!"});
-            })
-        // res.render('home');
+            .catch(next)
     }
 
     // [GET] /search
