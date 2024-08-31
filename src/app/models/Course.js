c0affa0dd953f32b18e5abbae9b8166f5e41ce39
huynhtri123 <huynhmtri1211@gmail.com>
2024-08-31 10:57:09 +0700

    Create new course (mongoose-slug-updater, auto generate create/updateAt, get image,video from youtube)

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: {type: String, default: '', maxlength: 255},
+    name: { type: String, default: '', required: true, },
     description: String,
     image: String,
-    createdAt: {type: Date, default: Date.now},
-    updatedAt: {type: Date, default: Date.now},
-  });
+    videoId: { type: String, required: true, },
+    slug: { type: String, slug: 'name', unique: true, },
+  }, {
+    timestamps: true,
+});
 
 module.exports = mongoose.model('Course', Course);
