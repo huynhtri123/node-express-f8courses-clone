d17c15f651be37693f39d6af02f88327e77931b9
huynhtri123 <huynhmtri1211@gmail.com>
2024-09-03 09:27:59 +0700

    Update course (method-override lib, custom func handlebars)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
 const path = require('path');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
 const db = require('./config/db');
+
+//HTTP methods override
+app.use(methodOverride('_method'));
 
 //connect to DB
 db.connect();
@@ -26,6 +30,9 @@ app.engine(
     'hbs',
     handlebars.engine({
         extname: 'hbs',
+        helpers: {
+            sum: (a,b) => a + b,
+        }   
     }),
 );
 app.set('view engine', 'hbs');
